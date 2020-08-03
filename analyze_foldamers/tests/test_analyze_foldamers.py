f7ec6f30f949aebb6b80063adddc260d6d30da9b
Chris Walker <Christopher.Walker-1@colorado.edu>
2020-08-03 14:24:28 -0600

    adding fresh molssi cookiecutter files

---
+++ analyze_foldamers/tests/test_analyze_foldamers.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the analyze_foldamers package.
+"""
+
+# Import package, test suite, and other packages as needed
+import analyze_foldamers
+import pytest
+import sys
+
+def test_analyze_foldamers_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "analyze_foldamers" in sys.modules
