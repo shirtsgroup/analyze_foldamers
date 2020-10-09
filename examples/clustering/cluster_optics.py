33cbf8109ecd61fed6ac08f0e274f45efc82abe4
Chris Walker <Christopher.Walker-1@colorado.edu>
2020-10-09 13:25:14 -0600

    adding example script for optics clustering

--- examples/clustering/cluster_optics.py
+++ examples/clustering/cluster_optics.py
@@ -17,19 +17,25 @@ for i in range(number_replicas):
     pdb_file_list.append("output/replica_%s.pdb" %(i+1))
 
 # Set clustering parameters
-n_clusters=4
+min_samples=5
 frame_start=0
 frame_stride=10
 frame_end=-1
 
-# Run KMeans clustering
-medoid_positions, cluster_size, cluster_rmsd, silhouette_avg = get_cluster_medoid_positions(
-    pdb_file_list,
-    cgmodel,
-    n_clusters=n_clusters,
-    frame_start=frame_start,
-    frame_stride=frame_stride,
-    frame_end=-1)
+# Run OPTICS density-based clustering
+    medoid_positions, cluster_sizes, cluster_rmsd, n_noise, silhouette_avg = get_cluster_medoid_positions_OPTICS(
+        dcd_file_list,
+        cgmodel,
+        min_samples=min_samples,
+        frame_start=frame_start,
+        frame_stride=frame_stride,
+        frame_end=-1,
+        output_format="dcd",
+        output_dir="output",
+        plot_silhouette=True,
+    )
 
-print(f'Cluster sizes: {cluster_size}')
+print(f'Cluster sizes: {cluster_sizes}')
 print(f'Cluster rmsd: {cluster_rmsd}')
+print(f"Fraction noise: {n_noise/(np.sum(cluster_sizes)+n_noise)}")
+print(f"Average silhouette score: {silhouette_avg}")
