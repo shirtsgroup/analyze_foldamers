868b81892276e8e92a53c96723b8fb295ca7fcc9
tlfobe <lennyfobe@gmail.com>
2021-10-01 17:29:37 -0600

    added example fitting helices found in CG models, uses MDTraj to get backbone atoms out

--- analyze_foldamers/parameters/helical_fitting_2.py
+++ analyze_foldamers/parameters/helical_fitting_2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import least_squares, minimize, basinhopping
 from mpl_toolkits import mplot3d
+from scipy.optimize._trustregion_constr.minimize_trustregion_constr import TERMINATION_MESSAGES
 
 
 def fit_helix_to_points(data_points, x0):
@@ -30,14 +31,18 @@ def fit_helix_to_points(data_points, x0):
     bounds = [(low, high) for low, high in zip(xmin, xmax)]
 
     cyl_results = minimize(cylinder_error, x0, args=(data_points), method="L-BFGS-B", bounds=bounds)
+    # print(cyl_results)
 
+    sse_cylinder = cyl_results.fun
+
+    r = np.sqrt(cyl_results.x[0])
     center = cyl_results.x[1:4]
     axis = cyl_results.x[4:]
     axis_norm = axis/np.sqrt(np.dot(axis, axis))
 
     # Rotate coordinate system to match tilt of helix
 
-    centered = data_points - center
+    centered_data_points = data_points - center
 
     cart_axis = np.array([0,0,1])
     ortho_norm = np.cross(axis_norm, cart_axis)
@@ -45,26 +50,56 @@ def fit_helix_to_points(data_points, x0):
 
     rotation_matrix = np.array([ortho_norm, np.cross(axis_norm, ortho_norm), axis_norm])
 
-    transformed_points = np.dot(data_points, rotation_matrix.transpose())
+    transformed_points = np.dot(centered_data_points, rotation_matrix.transpose())
+
+    lowest_z_val = np.min(transformed_points[:,2])
+
+    transformed_points[:,2] = transformed_points[:,2] - lowest_z_val
 
     # Fit points to helix equation
 
-    def helix_error(coeffs, data_points):
+    def linear_regress_phase(coeffs, data_points, radius):
+        # coeffs: 0:w 1:phi
+        w = coeffs[0]
+        phi = coeffs[1]
+        error = 0
+        phases = []
+        for i in range(data_points.shape[0]):
+            phase = np.arccos(data_points[i, 0]/radius)
+            phases.append(phase)
+            error += (phase - (w*data_points[i, 2] + phi))**2
+        # plt.figure()
+        # plt.scatter(data_points[:, 2], phases)
+        # x = np.linspace(np.min(data_points[:, 2]), np.max(data_points[:, 2]), 10)
+        # plt.plot(x, w*x+phi)
+        # plt.show()
+
+        return error
+
+    def helix_error(coeffs, data_points, radius):
         # coeffs: 0:radius, 1: w, 2:phi
-        radius = coeffs[0] # 1 parameter
-        w = coeffs[1] # 1 parameter
-        phi = coeffs[2] # 1 parameter
+        radius = radius # 1 parameter
+        w = coeffs[0] # 1 parameter
+        phi = coeffs[1] # 1 parameter
 
         error = 0
         for i in range(data_points.shape[0]):
             error += np.square(data_points[i,0] - radius*np.cos(w*data_points[i,2] + phi)) + np.square(data_points[i,1] - radius*np.sin(w*data_points[i,2] + phi))
-        print(error)
-        return error
+        
+        return error + w**2
 
-    helix_results = minimize(helix_error, np.array([2,1,1]), args=(data_points), method="L-BFGS-B")
-    print(helix_results)
+    xmin = [-np.inf, 0]
+    xmax = [np.inf, 2*np.pi]
 
-    return(*helix_results.x[:3], np.linalg.inv(rotation_matrix), center, axis_norm)
+    bounds = [(low, high) for low, high in zip(xmin, xmax)]
+    helix_results = basinhopping(helix_error, np.array([0,0]), minimizer_kwargs=dict(args=(transformed_points, r), bounds=bounds, method="L-BFGS-B"))
+
+    sse_helix = helix_results.fun
+
+
+    z_tot = np.max(transformed_points)
+
+    return(r, helix_results.x[0], helix_results.x[1], z_tot, np.linalg.inv(rotation_matrix), center, axis_norm, sse_helix, sse_cylinder)
 
 
 
@@ -97,7 +132,7 @@ def generate_test_helix(radius, pitch, res_per_turn, noise, n_points, phase_shif
 
 def main():
     # Generate sample helix and center at origin
-    test_helix = generate_test_helix(4, 4, 4.5, 0.05, 15, phase_shift = 0, rotation=[0,0,0])
+    test_helix = generate_test_helix(2, 2, 4.5, 0.1, 24, phase_shift = 0, rotation=[0,0,0])
     
     # Plot helix points
     fig = plt.figure()
@@ -106,25 +141,40 @@ def main():
     ax.plot3D(test_helix[:, 0], test_helix[:, 1], test_helix[:, 2])
 
     # Fit helix points to helix equation
-    x0 = np.array([1, 0, 0, 0, 0, -0.6, 1])
-    radius, w, phi, rotation, center, normal = fit_helix_to_points(test_helix, x0)
+    entries = []
+    RMSEs = []
+    for i in range(20):
+        x0 = np.array([1, 0, 0, 0, 0, 0, 0])
+        radius, w, phi, z_tot, rotation, center, normal, sse_helix, sse_cylinder = fit_helix_to_points(test_helix, x0)
+        RMSE_tot = np.sqrt(sse_cylinder/test_helix.shape[0]) + np.sqrt(sse_helix/test_helix.shape[0])
+        entries.append([radius, w, phi, z_tot, rotation, center, normal, sse_helix, sse_cylinder])
+        RMSEs.append(RMSE_tot)
+
+    i_min = RMSEs.index(np.min(RMSEs))
+
+    radius, w, phi, z_tot, rotation, center, normal, sse_helix, sse_cylinder = entries[i_min]
+    
 
     # Plot fitted helix
-    t = np.linspace(0, 20, 100)
+    t = np.linspace(0, z_tot, 100)
+    print(z_tot)
     fitted_helix = np.zeros([len(t), 3])
     fitted_helix[:, 0] = radius * np.cos(w*t + phi)
     fitted_helix[:, 1] = radius * np.sin(w*t + phi)
     fitted_helix[:, 2] = t
     fitted_helix = np.dot(fitted_helix, rotation.transpose())
+    fitted_helix = fitted_helix -  np.mean(fitted_helix, axis = 0)
     ax.plot3D(fitted_helix[:, 0], fitted_helix[:, 1], fitted_helix[:, 2])
 
 
 
     print("Helix Fit Summary")
-    print("Pitch", radius)
     print("Radius", radius)
-    print("Radius", radius)
-    print("Radius", radius)
+    print("Angular Frequency", w)
+    print("Phase Shift", phi)
+    print("RMSE Cylinder", np.sqrt(sse_cylinder/test_helix.shape[0]))
+    print("RMSE Helix", np.sqrt(sse_helix/test_helix.shape[0]))
+    
 
     # Show projection of helix on identified plane
     point = center
@@ -140,7 +190,6 @@ def main():
     projected_points = test_helix - dist[:, np.newaxis] * normal[np.newaxis, :]
     ax.scatter3D(projected_points[:, 0], projected_points[:, 1], projected_points[:, 2], c="black")
     ax.plot3D(projected_points[:, 0], projected_points[:, 1], projected_points[:, 2], "black")
-
 
     plt.show()
 
