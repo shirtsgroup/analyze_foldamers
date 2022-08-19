c76326cdf9f172753fcb1eb87aa20fb106bbfdfa
Christopher Walker <Christopher.Walker-1@colorado.edu>
2022-08-19 13:34:52 -0600

    update angle dist example with particle mapping dictionary

--- examples/angle_distributions/calc_angle_distributions.py
+++ examples/angle_distributions/calc_angle_distributions.py
@@ -16,6 +16,23 @@ traj_file = "simulation.pdb"
 # Load in a CGModel:
 cgmodel = pickle.load(open("stored_cgmodel.pkl","rb"))
 
-angle_hist_data = calc_bond_angle_distribution(cgmodel,traj_file)
-torsion_hist_data = calc_torsion_distribution(cgmodel,traj_file)
+# With a mapping dictionary, we can rename particle type names or 
+# combine multiple particle types into a common bonded type:
+
+particle_mapping = {
+    'bb': 'b',
+    'sc': 's',
+    }
+
+angle_hist_data = calc_bond_angle_distribution(
+    cgmodel,
+    traj_file,
+    particle_mapping=particle_mapping,
+    )
+    
+torsion_hist_data = calc_torsion_distribution(
+    cgmodel,
+    traj_file,
+    particle_mapping=particle_mapping,
+    )
 
